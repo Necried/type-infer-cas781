8ceea41e5d4e272d79c85a17c48f08d4b94cbb31
CSchank <chris@schankula.ca>
2024-07-19 19:08:49 -0400

    parsing for extended lets with tuple pattern matching

--- src/Repl.hs
+++ src/Repl.hs
@@ -28,7 +28,7 @@ main = runInputT defaultSettings (loop initState)
             let 
                 ins :: Expr -> Expr
                 ins expr = 
-                    foldr (\(name,(_, expr)) e -> Let name expr e) expr (env state)
+                    foldr (\(name,(_, expr)) e -> Let (VarPat name) expr e) expr (env state)
             minput <- getInputLine "λ "
             newEnv <- case minput of
                 Nothing -> pure state
@@ -43,7 +43,7 @@ main = runInputT defaultSettings (loop initState)
                                 case runTyInfer [] (ins expr) of
                                     Right (ty, ctx) ->
                                         outputStrLn $ show ty
-                                    Left err -> outputStrLn $ Text.unpack err
+                                    Left err -> outputStrLn $ show err
                             Left err ->
                                 outputStrLn $ show err
                         pure state
@@ -57,6 +57,40 @@ main = runInputT defaultSettings (loop initState)
                                     showToks tokens
                             Left err ->
                                 outputStrLn $ show err
+                        pure state
+                Just (':':'v':rest) -> 
+                    let 
+                        txt = Text.pack rest
+                    in do
+                        case runLex txt of
+                            Right tokens -> do
+                                outputStrLn $ showToks tokens
+                                case runLexParse txt of
+                                    Right (Left expr) -> do
+                                        outputStrLn $ show expr
+                                        outputStrLn $ show $ eval (toEnv $ env state) (ins expr)
+                                        pure state
+                                    Right res@(Right (name, expr, _)) -> do
+                                        outputStrLn $ show res
+                                        case runTyInfer [] (ins expr) of
+                                            Right (ty, ctx) -> 
+                                                let
+                                                    val = eval (toEnv $ env state) (ins expr)
+                                                in do
+                                                    outputStrLn $ show ctx
+                                                    outputStrLn $ show val
+                                                    pure $ state 
+                                                        { env = env state ++ [(name, (val, expr))]
+                                                        }
+                                            Left err -> do
+                                                outputStrLn $ show err
+                                                pure state
+                                    Left err -> do
+                                        outputStrLn $ show err
+                                        pure state
+                            Left err -> do
+                                outputStrLn $ show err
+                                pure state
                         pure state
                 Just str ->
                     let 
@@ -78,7 +112,7 @@ main = runInputT defaultSettings (loop initState)
                                                 { env = env state ++ [(name, (val, expr))]
                                                 }
                                     Left err -> do
-                                        outputStrLn $ Text.unpack err
+                                        outputStrLn $ show err
                                         pure state
                             Left err -> do
                                 outputStrLn $ show err
